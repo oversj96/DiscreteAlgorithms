6d4dd3b67b8a71b00ce578862e7bc4fa0cda95a6
oversj96 <45669103+oversj96@users.noreply.github.com>
2019-09-19 22:07:07 -0500

    updated test case

--- public_key_encryption.py
+++ public_key_encryption.py
@@ -37,6 +37,19 @@ def public_key_decrypt(pq, e, t):
     return message
 
 
+def find_key(input_dict, value):
+    return next((k for k, v in input_dict.items() if v == value), None)
+
+
 if __name__ == "__main__":
+    # Test encryption
     print(public_key_encrypt(1901, 4507, 10, 17))
-    print(public_key_decrypt(247, 5, [161, 212, 161, 212, 164, 119, 203, 32, 97, 32]))
+    # Test decryption
+    decrypted_info = public_key_decrypt(247, 5, [161, 212, 161, 212, 164, 119, 203, 32, 97, 32])
+    alpha_dict = {'a': 2, 'b': 3, 'c': 4, 'd': 5, 'e': 6, 'f': 7, 'g': 8, 'h': 9, 'i': 10, 'j': 11, 'k': 12, 'l': 13,
+                  'm': 14, 'n': 15, 'o': 16, 'p': 17, 'q': 18, 'r': 19, 's': 20, 't': 21, 'u': 22, 'v': 23, 'w': 24,
+                  'x': 25, 'y': 26, 'z': 27}
+    message = []
+    for info in decrypted_info:
+        message.append(find_key(alpha_dict, info))
+    print(' '.join(message))
