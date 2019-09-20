729a939c8fbfb3a4df566a7dd597a0775f8cc910
oversj96 <45669103+oversj96@users.noreply.github.com>
2019-09-19 21:42:10 -0500

    completed implementation of public key encryption

--- linear_combination.py
+++ linear_combination.py
@@ -7,7 +7,7 @@ __copyright__ = "oversj86.github.io"
 
 
 
-def gcd_linear_combo(dividend, divisor):
+def gcd_linear_combo(dividend, divisor, verbose=False):
     remainder = dividend % divisor
     o_dividend = dividend
     o_divisor = divisor
@@ -31,46 +31,53 @@ def gcd_linear_combo(dividend, divisor):
     right_factor = divisors[i]
     right_multiplier = quotients[i]
     remainder = remainders[i]
-    print(f"{remainder} = ({left_multiplier})({left_factor}) - ({right_multiplier})({right_factor})")
+    if verbose:
+        print(f"{remainder} = ({left_multiplier})({left_factor}) - ({right_multiplier})({right_factor})")
     while i is not 0:
         if right_factor is remainders[i-1]:
             next_left_mult = 1
             next_right_mult = quotients[i-1]
             next_left_factor = dividends[i-1]
             next_right_factor = divisors[i-1]
-            print(f"{remainder} = ({left_multiplier})({left_factor}) - ({right_multiplier})({right_factor})"
-                  f" (substitute {right_factor}=({next_left_mult})({next_left_factor}) - "
-                  f"({next_right_mult})({next_right_factor}))")
-            print(f"{remainder} = ({left_multiplier})({left_factor}) - ({right_multiplier})"
-                  f"[({next_left_mult})({next_left_factor}) - ({next_right_mult})({next_right_factor})]")
+            if verbose:
+                print(f"{remainder} = ({left_multiplier})({left_factor}) - ({right_multiplier})({right_factor})"
+                      f" (substitute {right_factor}=({next_left_mult})({next_left_factor}) - "
+                      f"({next_right_mult})({next_right_factor}))")
+                print(f"{remainder} = ({left_multiplier})({left_factor}) - ({right_multiplier})"
+                      f"[({next_left_mult})({next_left_factor}) - ({next_right_mult})({next_right_factor})]")
             next_right_mult *= right_multiplier
             next_left_mult *= right_multiplier
-            print(f"{remainder} = ({left_multiplier})({left_factor}) - ({next_left_mult})({next_left_factor})"
-                  f" + ({next_right_mult})({next_right_factor})")
+            if verbose:
+                print(f"{remainder} = ({left_multiplier})({left_factor}) - ({next_left_mult})({next_left_factor})"
+                      f" + ({next_right_mult})({next_right_factor})")
             left_multiplier += next_right_mult
             right_multiplier = next_left_mult
             right_factor = next_left_factor
-            print(f"{remainder} = ({left_multiplier})({left_factor}) - ({right_multiplier})({right_factor})")
+            if verbose:
+                print(f"{remainder} = ({left_multiplier})({left_factor}) - ({right_multiplier})({right_factor})")
         elif left_factor is remainders[i-1]:
             next_left_mult = 1
             next_right_mult = quotients[i-1]
             next_left_factor = dividends[i-1]
             next_right_factor = divisors[i-1]
-            print(f"{remainder} = ({left_multiplier})({left_factor}) - ({right_multiplier})({right_factor})"
-                  f" (substitute {left_factor}=({next_left_mult})({next_left_factor}) - "
-                  f"({next_right_mult})({next_right_factor}))")
-            print(f"{remainder} = ({left_multiplier})[({next_left_mult})({next_left_factor}) - "
-                  f"({next_right_mult})({next_right_factor})]"
-                  f" - ({right_multiplier})({right_factor})")
+            if verbose:
+                print(f"{remainder} = ({left_multiplier})({left_factor}) - ({right_multiplier})({right_factor})"
+                      f" (substitute {left_factor}=({next_left_mult})({next_left_factor}) - "
+                      f"({next_right_mult})({next_right_factor}))")
+                print(f"{remainder} = ({left_multiplier})[({next_left_mult})({next_left_factor}) - "
+                      f"({next_right_mult})({next_right_factor})]"
+                      f" - ({right_multiplier})({right_factor})")
             next_right_mult *= left_multiplier
             next_left_mult *= left_multiplier
-            print(f"{remainder} = ({next_left_mult})({next_left_factor}) - {next_right_mult}({next_right_factor})"
-                  f" - ({right_multiplier})({right_factor})")
+            if verbose:
+                print(f"{remainder} = ({next_left_mult})({next_left_factor}) - {next_right_mult}({next_right_factor})"
+                      f" - ({right_multiplier})({right_factor})")
             right_multiplier += next_right_mult
             left_factor = next_left_factor
-            print(f"{remainder} = ({left_multiplier})({left_factor}) - ({right_multiplier})({right_factor})")
+            if verbose:
+                print(f"{remainder} = ({left_multiplier})({left_factor}) - ({right_multiplier})({right_factor})")
         i -= 1
-
+    return left_factor, right_multiplier
 
 
 if __name__ == "__main__":
--- public_key_encryption.py
+++ public_key_encryption.py
@@ -5,4 +5,38 @@
 __author__ = "Justin Overstreet"
 __copyright__ = "oversj86.github.io"
 
-# Placeholder
+
+from fermat_factorization import fermat_factorization
+from linear_combination import gcd_linear_combo
+
+
+def decipher_private_key(multiple, e):
+    factor, quotient = gcd_linear_combo(multiple, e)
+    return factor - quotient
+
+
+def public_key_encrypt(p, q, t, e):
+    decremented_p = p - 1
+    decremented_q = q - 1
+    multiple = decremented_p * decremented_q
+    d = decipher_private_key(multiple, e)
+    if type(t) is list:
+        message = [(m ** e) % (p*q) for m in t]
+    else:
+        message = (t ** e) % (p*q)
+    return message
+
+
+def public_key_decrypt(pq, e, t):
+    p, q = fermat_factorization(pq)
+    d = decipher_private_key((p-1)*(q-1), e)
+    if type(t) is list:
+        message = [(m ** d) % (p*q) for m in t]
+    else:
+        message = (t ** d) % (p*q)
+    return message
+
+
+if __name__ == "__main__":
+    print(public_key_encrypt(1901, 4507, 10, 17))
+    print(public_key_decrypt(247, 5, [161, 212, 161, 212, 164, 119, 203, 32, 97, 32]))
